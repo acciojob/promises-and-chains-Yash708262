e902960b6c6b87d94a62a8f518122597075fb805
Yash708262 <yash708262@gmail.com>
2025-08-24 21:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("voteForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // stop page reload
+
+  const name = document.getElementById("name").value.trim();
+  const age = document.getElementById("age").value.trim();
+
+  // validation
+  if (!name || !age) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Promise logic
+  const checkEligibility = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(age) > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  checkEligibility
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+});
